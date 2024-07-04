aa691d196395264092044fd73abe574be216eff5
iamfyke <fyketonel@gmail.com>
2024-07-04 14:56:44 +0800

    variables

--- Main.dart
+++ Main.dart
@@ -3,5 +3,8 @@ void main() {
   int yearBorn = 2004;
   double favoriteNumber = 22.2;
   List<String> nameLists = ['Fyke', 'Simon', 'V.', 'Tonel'];
-  print(name);
+  assert(yearBorn == 2004);
+  print('Name of the student: $name'
+      + '\nBirth year: $yearBorn'
+      + '\nFavorite number: $favoriteNumber');
 }
