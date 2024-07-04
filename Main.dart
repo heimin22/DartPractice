94705700b6f25d0faa8f9de61f8e29366484ead6
iamfyke <fyketonel@gmail.com>
2024-07-04 15:23:35 +0800

    variables

--- Main.dart
+++ Main.dart
@@ -6,13 +6,15 @@ void main() {
   name = 'Fyke';
   yearBorn = 2004;
   favoriteNumber = 22.2;
+  int intFaveNum = favoriteNumber.toInt();
   // List<String> nameLists = ['Fyke', 'Simon', 'V.', 'Tonel'];
   assert(yearBorn == 2004);
 
   if (name != null) {
     print('Name of the student: $name'
         + '\nBirth year: $yearBorn'
-        + '\nFavorite number: $favoriteNumber');
+        + '\nFavorite number: $favoriteNumber'
+        + '\nInteger favorite number: $intFaveNum');
   }
   else {
     print('Unknown student');
