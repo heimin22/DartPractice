6266470043a28cd5c56b41c8b2ee24b85522847f
iamfyke <fyketonel@gmail.com>
2024-07-05 14:41:51 +0800

    bored kasi ako

---
+++ OpenGame.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'dart:math';
+
+late int randomNumber;
+late int chosenNumber;
+
+void main() {
+  Random random = new Random();
+  stdout.write("Pili ka ng number from 1 to 5");
+
+}
