0d87e6b82717d8d815538e0543a202bff01ba41c
iamfyke <fyketonel@gmail.com>
2024-07-10 14:32:05 +0800

    bored kasi ako

--- Main.dart
+++ Main.dart
@@ -1,5 +1,6 @@
 void main() {
-  int x = 23, y = 21, z = 44;
-  print('Ito ay isang equation motherfucker: ${x + y}'
-      + '\nSi 23 + 21 ba ay equals to 44? ${(x + y == z) ? 'Oo' : 'Hindi'}');
+  int x = 23, y = 21, z = 44, sum = x % y;
+  print('Ito ay isang equation motherfucker (23 % 21): ${sum}\n'
+      'Si 23 % 21 ba ay equals to 44? ${(sum == z) ? 'Oo naman equal siya sa 44'
+          : (x + y != sum) ? 'Hindi pare kasi ang sagot niyan is: ${sum}' : 'Ewan ko pre kung anong sagot'}');
 }
