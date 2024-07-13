72fd777a7f61bee8e90594ebab5b3aa524e88d08
iamfyke <fyketonel@gmail.com>
2024-07-13 15:28:57 +0800

    bored kasi ako

--- Main.dart
+++ Main.dart
@@ -1,12 +1,17 @@
 import 'dart:io';
 
-
 void main() {
-  stdout.write("Enter your name: ");
-  String? name = stdin.readLineSync();
+  int number = 3;
+  double number2 = number.toDouble();
+  int number3 = number2.toInt();
+  String string4 = number3.toString();
+  int number5 = int.parse(string4);
+  List<dynamic> numbers = [1, 2, 3, 4];
+  List<int> numbersMore =  numbers.cast<int>();
 
-  stdout.write("\nEnter your age:");
-  int? age = int.tryParse(stdin.readLineSync()!);
-
-  print("${name} is ${age} years old");
+  print("${number2}"
+      "\n${number3}"
+      "\n${string4}"
+      "\n${number5}"
+      "\n${numbersMore}");
 }
