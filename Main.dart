05cbbfd7ef164afb8454835dc402a74a3c18ac63
iamfyke <fyketonel@gmail.com>
2024-07-15 14:09:06 +0800

    bored kasi ako

--- Main.dart
+++ Main.dart
@@ -1,17 +1,9 @@
-import 'dart:io';
-
 void main() {
-  int number = 3;
-  double number2 = number.toDouble();
-  int number3 = number2.toInt();
-  String string4 = number3.toString();
-  int number5 = int.parse(string4);
-  List<dynamic> numbers = [1, 2, 3, 4];
-  List<int> numbersMore =  numbers.cast<int>();
+  int a = 2, b = 2;
 
-  print("${number2}"
-      "\n${number3}"
-      "\n${string4}"
-      "\n${number5}"
-      "\n${numbersMore}");
+  print("Before ${a}");
+
+  a &= b;
+
+  print("After: ${b}");
 }
