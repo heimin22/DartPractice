2c6bfa05a8a1b69b10a9be28b29c989b24734ca6
iamfyke <fyketonel@gmail.com>
2024-07-16 15:02:15 +0800

    bored kasi ako

--- Main.dart
+++ Main.dart
@@ -1,6 +1,15 @@
 import 'dart:io';
 
 void main() {
+
+  List<String> nameList = ["Fyke", "Simon", "Visco", "Tonel"];
+
+  for (String listNames in nameList) {
+    stdout.write(listNames + ' ');
+  }
+
+  print('\n');
+
   Map<String, int> namesNumbers = {
     'Fyke' : 22,
     'Simon' : 2
